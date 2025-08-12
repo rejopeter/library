7936eb489274ec5f68db743abccc6bb2acc14f61
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-08-13 05:21:15 +0530

    skeleton done

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+
